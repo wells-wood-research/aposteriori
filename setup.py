27de7927cd7a833d78cfc74ecf35628bf2ece12e
Leonardo Castorina <leonardo.castorina98@gmail.com>
2021-09-28 09:40:44 +0100

    Fix setup versioning

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup, find_packages
-from src.aposteriori.config import MAKE_FRAME_DATASET_VER
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name="aposteriori",
-    version=MAKE_FRAME_DATASET_VER,
+    version="2.0.0",
     author="Wells Wood Research Group",
     author_email="chris.wood@ed.ac.uk",
     description="A library for the voxelization of protein structures for protein design.",
