2aa4fe93bc18e5bf525fb1083ef7b747a8b3ea13
Leonardo Castorina <leonardo.castorina98@gmail.com>
2021-11-13 16:07:14 +0000

    Fix pdb chain selection bug + typo

--- tests/test_create_frame_data_set.py
+++ tests/test_create_frame_data_set.py
@@ -469,7 +469,7 @@ def test_download_pdb_from_csv_file():
     assert TEST_DATA_DIR / "1qys.pdb1" in test_file_paths, f"Expected to find {TEST_DATA_DIR / '3qy1A.pdb1'} as part of the generated paths."
     assert TEST_DATA_DIR / "6ct4.pdb1" in test_file_paths, f"Expected to find {TEST_DATA_DIR / '6ct4.pdb1'} as part of the generated paths."
     assert (TEST_DATA_DIR / "1qys.pdb1").exists(), f"Expected download of 1QYS to return PDB file"
-    assert (TEST_DATA_DIR / "3qy1A.pdb1").exists(), f"Expected download of 1QYS to return PDB file"
+    assert (TEST_DATA_DIR / "3qy1A.pdb1").exists(), f"Expected download of 3QYA to return PDB file"
     assert (TEST_DATA_DIR / "6ct4.pdb1").exists(), f"Expected download of 6CT4 to return PDB file"
 
 
